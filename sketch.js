5b115245964c56d4492ebabd77a19433327c60df
rory114 <rorykeelan@Rorys-MacBook-Pro-3.local>
2022-05-12 15:56:08 -0500

    fallings toppings update

--- sketch.js
+++ sketch.js
@@ -1,23 +1,31 @@
 let active_fallers = [];
 let burger_stack = [];
+let toppings_allowed = [];
+let lettuce_image, tomato_image, patty_image, onion_image, egg_image;
 let skip_frame_counter = 0;
 const faller_width = 100;
-const faller_height = 30;
+const faller_height = 70;
 const catcher_width = 100;
 let bottom_border_factor = 20;
 let catcher_x;
-let play_again_button;
 
-let colorArray = ["#ffd046","#ef233c","#235789","#7ee081","#98c1d9","#6b5ca5","#028A42","#009fb7","#f9a03f","#00171f"]
+// toppings class to store height and width of each topping
+class Topping {
+    constructor(image, height, width) {
+        this.image = image;
+        this.height = height;
+        this.width = width;
+    }
+}
 
 // ellipise falling from top of the screen to the bottom    
 // random color, speed, and x cordinate assigned
 class Faller {
-    constructor( x_pos, y_pos, y_speed, color) {
+    constructor( x_pos, y_pos, y_speed, topping ) {
         this.x_pos = x_pos;
         this.y_pos = y_pos;
         this.y_speed = y_speed;
-        this.color = color;
+        this.topping = topping;
     }
     // move faller's position based on speed
     move() {
@@ -34,16 +42,16 @@ class Faller {
     // display faller in the current position
     display() {
         imageMode(CENTER);
-        image(lettuce_topping, this.x_pos, this.y_pos, faller_width, faller_height);
+        image(this.topping.image, this.x_pos, this.y_pos, this.topping.width, this.topping.height);
     }
     // check if faller is colliding with the top of the stack
     // if so add to stack and remove from active fallers
     isColliding() {
         // check x_pos value is with X pixels of catcher
-        if( abs(this.x_pos - catcher_x) < faller_width/2 ) {
+        if( abs(this.x_pos - catcher_x) < this.topping.height/2 ) {
             // check y_pos value is within X pixels of catcher
-            let stack_height = burger_stack.length * faller_height * 1/3;
-            if( abs(this.y_pos - (windowHeight - bottom_border_factor - stack_height) ) < faller_height/2 ) {
+            let stack_height = burger_stack.length * this.topping.height * 1/3;
+            if( abs(this.y_pos - (windowHeight - bottom_border_factor - stack_height) ) < this.topping.height/2 ) {
                 // grab index of faller colliding
                 const index_of_faller = active_fallers.indexOf(this);
 
@@ -59,15 +67,26 @@ class Faller {
 
 // load background image
 function preload() {
-    lettuce_topping = loadImage("images/lettuce_leaf.png")
+    lettuce_image = loadImage("images/lettuce_leaf.png");
+    tomato_image = loadImage("images/tomato_slice.png");
+    patty_image = loadImage("images/burger_patty.png");
+    egg_image = loadImage("images/fried_egg.png");
+    onion_image = loadImage("images/onion_slice.png");
 }
 
 // set p5 canvas
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    lettuce_topping.resize(faller_width, faller_height);
     frameRate(30);
     catcher_x = windowWidth / 2;
+
+    // set up toppings array
+    toppings_allowed.push( new Topping(lettuce_image, faller_height, faller_width) );
+    toppings_allowed.push( new Topping(tomato_image, faller_height, faller_width) );
+    toppings_allowed.push( new Topping(patty_image, faller_height, faller_width) );
+    toppings_allowed.push( new Topping(egg_image, faller_height, faller_width) );
+    toppings_allowed.push( new Topping(onion_image, faller_height, faller_width) );
+
 }
 
 // call this function each new frame
@@ -87,13 +106,15 @@ function addFaller() {
     let y_pos = -(faller_height/2);
     let y_speed = Math.floor( Math.random()* 7) + 3;
     
-    // grab random color
-    let color_index = Math.floor(Math.random()*colorArray.length);
-    let color = colorArray[color_index];
+    // grab random topping
+    let topping_index = Math.floor( Math.random()* toppings_allowed.length );
+    let topping_to_add = toppings_allowed[topping_index];
 
-    // create faller object and push to array
-    let new_faller = new Faller( x_pos, y_pos, y_speed, color);
-    active_fallers.push(new_faller);
+
+    // create and add faller
+    let faller_to_add = new Faller( x_pos, y_pos, y_speed, topping_to_add );
+    active_fallers.push(faller_to_add);
+
 }
 
 // runs fallers and displays the stack
