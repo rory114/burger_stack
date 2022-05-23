998df985726a4b61a9bc3277a1218c4aa75a40a1
rory114 <rorykeelan@Rorys-MacBook-Pro-3.local>
2022-05-23 12:19:24 -0500

    pre isOrderFilled() deleted

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ let current_order_count = {};
 let topping_image_access = {};
 let min_order_toppings = 5;
 let max_order_toppings = 10;
+let order_filled = false;
 
 
 // toppings class to store height and width of each topping
@@ -71,8 +72,59 @@ class Faller {
 
                 // add topping to stack
                 burger_stack.push(this);
+
+                // check if topping is top_bun
+                if(!checkIfTopBun(this.topping)) {
+                    // check if topping is in order
+                    checkIfInOrder(this.topping)
+                } else {
+                    // topping is top bun
+                    // execute end function
+                    topBunHit();
+                }
             }
         }
+    }
+}
+// top bun has been found
+// check if order matches burger_stack
+// if yes add point
+function topBunHit() {
+    // copy burger_stack and sort
+    let burger_stack_copy = [];
+    for( let i = 0; i < burger_stack.length; i++ ) {
+        burger_stack_copy[i] = burger_stack[i].topping.name;
+    }
+    burger_stack_copy.sort();
+    
+    // copy order and sort
+    let current_order_copy = [];
+    for( let i = 0; i < current_order.length; i++ ) {
+        current_order_copy[i] = current_order[i].name;
+    }
+    current_order_copy.sort();
+
+    if( current_order_copy == burger_stack_copy )
+        console.log("FILLED")
+}
+
+// checks if the given topping is the top bun
+// if so check if burger_stack matches order
+function checkIfTopBun( topping ) {
+    if( topping.name == "top_bun" ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// check if a given faller.topping is needed to complete the order
+// if so decrement the counter
+function checkIfInOrder( topping ) {
+
+    // check if topping is needed
+    if( topping.name in current_order_count && current_order_count[topping.name] != 0) {
+        current_order_count[topping.name]--;
     }
 }
 
@@ -121,14 +173,7 @@ function setup() {
 
     /* FUCK AROUND ZONE */
 
-    
-    getOrder();
-
-    console.log( "Order Toppings" )
-    for( let j = 0; j < current_order.length; j++ ) {
-        console.log(current_order[j].name)
-    }
-    console.log("")    
+    //getOrder();
 
     /* FUCK AROUND ZONE */
 
@@ -235,9 +280,19 @@ function onMouseMove() {
 function runGame() {
 
     // display score
-    text("Score: " + score, windowWidth/2, windowHeight/2 )
+    text("Score: " + score, 40, faller_height * 2 )
 
+    // if order is filled get new order
+    if( order_filled ) {
+        getOrder()
+        score++;
+    }
+
+    // display current order to fill
     displayOrderToFill()
+
+    // check if order is filled
+    isOrderFilled()
 }
 
 // fill current_order with new random order
@@ -273,14 +328,7 @@ function getOrder() {
             return -1
     });
 
-
-    // store current_order frequency for displaying
-    for( let i = 0; i < current_order.length; i++ ) {
-        if( current_order_count[current_order[i].name] )
-            current_order_count[current_order[i].name] += 1;
-        else
-            current_order_count[current_order[i].name] = 1;
-    }
+    countOrderToppings()
 }
 
 function displayOrderToFill() {
@@ -288,11 +336,50 @@ function displayOrderToFill() {
     // store width of one order item
     let topping_order_width = windowWidth / Object.keys(current_order_count).length;
 
-    // display each topping
+    // display each topping and topping count
     let i = 0;
     for( const topping in current_order_count ) {
         text(current_order_count[topping] + " x ", (topping_order_width * i) + topping_order_width/2 - faller_width/2 - 20, faller_height)
         image( topping_image_access[topping], (topping_order_width * i) + topping_order_width/2, faller_height, faller_width, faller_height );
         i++;
+    }
+}
+
+function isOrderFilled() {
+    // loop through current_order_count and check
+    // that all counts are 0
+    order_filled = true;
+    for( const topping in current_order_count ) {
+        if( current_order_count[topping] != 0 )
+            order_filled = false;
+    }
+}
+
+
+// Reset burger stack and current_order_counts 
+function mousePressed() {
+    burger_stack = [];
+
+    // reset current_order_count to starting order
+    countOrderToppings();
+}
+
+// takes the current order stored in current_order
+// and saves the frequencies of each to a property
+// of current_order_count
+function countOrderToppings() {
+
+    // reset all values in current_order_count to 0
+    for( const topping in current_order_count ) {
+        current_order_count[topping] = 0;
+    }
+
+
+    // store current_order frequency in current_order_count
+    for( let i = 0; i < current_order.length; i++ ) {
+        if( current_order_count[current_order[i].name] )
+            current_order_count[current_order[i].name] += 1;
+        else
+            current_order_count[current_order[i].name] = 1;
     }
 }
